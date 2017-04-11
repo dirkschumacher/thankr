4348446df43d29bcba7bcc9e9c4299f536d61087
Dirk Schumacher <mail@dirk-schumacher.net>
2017-04-11 23:00:06 +0200

    Add package doc

---
+++ R/thankr-package.R
@@ -0,0 +1,9 @@
+#' Find Out Who Wrote the Packages you Use
+#'
+#' Find out who maintains the packages you use in
+#' your current session or in your package library.
+#'
+#' @docType package
+#' @keywords package
+#' @name thankr
+NULL
